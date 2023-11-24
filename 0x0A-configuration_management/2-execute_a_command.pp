2a240137a91ad3f1bb61424e3b27a3ee83318c7b
Asega-bryan07 <Almasibryan7@gmail.com>
2023-11-25 00:31:23 +0300

    DEVOPS Configuration Management

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,10 @@
+# Using Puppet, create a manifest that kills a process named killmenow.
+# Requirements:
+# Must use the exec Puppet resource
+# Must use pkill
+# Example:
+# Terminal #0 - starting my process
+exec { 'kill':
+command => 'pkill -f killmenow',
+path => ['/usr/bin', '/usr/bin']
+}
