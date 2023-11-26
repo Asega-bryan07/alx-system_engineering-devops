e9e48c6170b03b9f70b3b58f785d0fb91023f5b7
Asega-bryan07 <Almasibryan7@gmail.com>
2023-11-26 21:29:44 +0300

    Secure Shell

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,13 @@
+# puppet script to create a ssh config file
+# and make changes to our configuration file
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file',
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => 'IdentifyFile ~/.ssh/school',
+}
